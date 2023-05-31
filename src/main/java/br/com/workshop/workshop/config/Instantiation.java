421853c54c2ebf5b9916e2d0da5c8c1bab8398f7
Jaime Cardoso <70758067+Jaime-Cardoso@users.noreply.github.com>
2023-05-31 15:49:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/br/com/workshop/workshop/config/Instantiation.java
+++ src/main/java/br/com/workshop/workshop/config/Instantiation.java
@@ -2,6 +2,7 @@ package br.com.workshop.workshop.config;
 
 import br.com.workshop.workshop.domain.Post;
 import br.com.workshop.workshop.domain.User;
+import br.com.workshop.workshop.dto.AuthorDTO;
 import br.com.workshop.workshop.repository.PostRepository;
 import br.com.workshop.workshop.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -29,11 +30,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo abraços", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São paulo abraços",new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/br/com/workshop/workshop/dto/AuthorDTO.java
@@ -0,0 +1,32 @@
+package br.com.workshop.workshop.dto;
+
+import br.com.workshop.workshop.domain.User;
+
+import java.io.Serializable;
+import java.security.PrivateKey;
+
+public class AuthorDTO implements Serializable {
+    private  String id;
+    private  String name;
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
